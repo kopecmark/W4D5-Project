186ba60c870e4f082d7e218287bd1c575791de61
kopecmark <mark.kopec@gmail.com>
2018-11-16 15:37:04 -0800

    Add applicatioin controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+
+  def login(user)
+    # establish a relationship btw browser & server by setting the same session_token
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: sessions[:session_token])
+  end
+
+  def log_out
+    # reset the user's session token
+    # reset the browser's session hash
+    # set current_user to nil
+
+    self.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def require_sign_in
+    redirect_to new_user_url unless signed_in?
+    # change to new_session_url after we establish session
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,22 +9,10 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe 'POST #create' do
-    # let (:user_params) do
-    #   { user: {
-    #       username: 'harry_potter',
-    #       email: 'harry@hogwarts.com',
-    #       age: '25',
-    #       password: 'abcdef'
-    #     }
-    #   }
-    # end
 
     context 'with valid params' do
       it 'logs in the user' do
         post :create, params: { user: { username: 'harry_potter', password: 'abcdef' } }
-
-        # Use this after adding let above
-        # post :create, params: user_params
 
         user = User.find_by(username: 'harry_potter')
         expect(session[:session_token]).to eq(user.session_token)
@@ -33,8 +21,6 @@ RSpec.describe UsersController, type: :controller do
       it 'redirects to the user\'s show page' do
         post :create, params: { user: { username: 'harry_potter', password: 'abcdef' } }
 
-        #Use this after adding let above
-        # post :create, params: user_params
 
         user = User.find_by(username: 'harry_potter')
         expect(response).to redirect_to(user_url(user))
